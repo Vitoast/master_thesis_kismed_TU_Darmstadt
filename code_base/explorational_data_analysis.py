d5af1d90ca49173d069661fa432f58b362d54bae
Tobias Steinbach <tobias.steinbach@stud.tu-darmstadt.de>
2024-04-16 12:22:59 +0200

    Extended EDA.

--- code_base/explorational_data_analysis.py
+++ code_base/explorational_data_analysis.py
@@ -2,6 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 import re
+import seaborn as sns
+import pandas as pd
+from collections import Counter
+
 
 
 def clean_feature_name(feature_name):
@@ -23,7 +27,7 @@ def explore_data(data_dictionary, output_directory):
 
     # Create file to store results
     stats_file_path = os.path.join(output_directory, 'exploration_results.txt')
-    outcome_counter = 0
+    feature_count = 0
     with open(stats_file_path, 'w') as stats_file:
         # Iterate over each feature
         for feature_name, feature_data in data_dictionary.items():
@@ -32,35 +36,55 @@ def explore_data(data_dictionary, output_directory):
                 # Clean data from nan
                 cleaned_data = [x for x in feature_data if not np.isnan(x)]
 
-                # Plot a histogram of the data
-                plt.hist(cleaned_data, bins=100, color='green', edgecolor='black')
-                plt.title(f'Histogram of feature {feature_name}')
-                plt.xlabel('Value')
-                plt.ylabel('Occurrences')
-                plt.grid(True)
-                plt.tight_layout()
-
                 cleaned_feature_name = clean_feature_name(feature_name)
 
-                # Determine subfolder based on feature name
-                if outcome_counter < 5:
-                    subfolder = outcome_directory
-                else:
-                    if cleaned_feature_name.endswith('PRE') or cleaned_feature_name.endswith('POST'):
-                        subfolder = clinical_marker_directory
-                    else:
-                        subfolder = clinical_data_directory
+                classification = all(x in [0, 1] for x in cleaned_data)
 
-                # Save histogram as PNG file
-                output_file_path = os.path.join(subfolder, f'hist_{cleaned_feature_name}.png')
+                # Determine subfolder based on feature name
+                if classification:
+                    counts = Counter(cleaned_data)
+                    labels = ['No', 'Yes']
+                    sizes = [counts[0], counts[1]]
+
+                    # Creating the pie chart
+                    plt.figure(figsize=(8, 5))
+                    plt.bar(labels, sizes, color=['green', 'red'])
+                    plt.title(f'Ratio of {feature_name}')
+                    plt.xlabel('Outcome')
+                    plt.ylabel('Occurences')
+
+                    if feature_count < 5:
+                        output_file_path = os.path.join(outcome_directory, f'bar_{cleaned_feature_name}.png')
+                    else:
+                        output_file_path = os.path.join(clinical_data_directory, f'bar_{cleaned_feature_name}.png')       
+
+                else:
+                    # Plot a histogram of the data
+                    plt.figure(figsize=(8, 4))
+                    sns.histplot(cleaned_data, bins=20, kde=True, color='green', edgecolor='black')
+                    plt.title(f'Histogram of feature {feature_name}')
+                    plt.xlabel('Value')
+                    plt.ylabel('Occurrences')
+                    plt.grid(True)
+                    plt.tight_layout()
+
+                    if cleaned_feature_name.endswith('PRE') or cleaned_feature_name.endswith('POST'):
+                        output_file_path = os.path.join(clinical_marker_directory, f'hist_{cleaned_feature_name}.png')
+                    else:
+                        output_file_path = os.path.join(clinical_data_directory, f'hist_{cleaned_feature_name}.png')                    
+                
+                # Save plot as PNG file
                 plt.savefig(output_file_path)
                 plt.close()
+                feature_count += 1
 
                 # Calculate mean and variance
                 mean = np.mean(cleaned_data)
                 var = np.var(cleaned_data)
+                median = np.median(cleaned_data)
+                minimum = min(cleaned_data)
+                maximum = max(cleaned_data)
 
                 # Save mean and variance to text file
-                stats_file.write(f'{feature_name}: Mean = {mean}, Variance = {var}, Data Points: {len(cleaned_data)}\n')
+                stats_file.write(f'{feature_name}: Mean: {mean}, Median: {median}, Variance: {var}, Data Points: {len(cleaned_data)}, Min Value: {minimum}, Max Value: {maximum}\n')
 
-            outcome_counter += 1
